c579d49a4e2db5a611c91da65e8911b5c60fec66
mgreaves-anz <matgreaves.eng@gmail.com>
2022-09-22 09:39:03 +1000

    Proxy only handles a single connection, logging prefixes

---
+++ README.md
@@ -0,0 +1,11 @@
+# tcp-proxy
+
+A TCP proxy.
+
+## Usage
+`tcp-proxy -src localhost:8080 -dst localhost:8081 -debug true`
+
+## TODO
+
+- [] add logging for inbound connections
+- [] add record errors and log errors on connection closed
--- main.go
+++ main.go
@@ -7,73 +7,101 @@ import (
 	"io"
 	"log"
 	"net"
-	"os"
-	"sync"
 )
 
 var src *string = flag.String("src", "localhost:8080", "local address to listen on")
 var dst *string = flag.String("dst", "localhost:8081", "remove address to forward to")
+var debug *bool = flag.Bool("debug", false, "debug connections, logging connection details to stdout")
 
 // Proxy forwards TCP traffic from src to dst
 type Proxy struct {
-	src string
-	dst string
-	log log.Logger
+	dst     string
+	srcConn net.Conn
+	dstConn net.Conn
+	debug   bool
+	done    chan (bool)
+	log     *log.Logger
 }
 
-// TODO: move main to cmd directory, refactor proxy to just handle single proxy connection
+// TODO: move main to cmd directory
 func main() {
 	// listen for new inbound connections
 	// handle each each connection in a goroutine
 	// need to consider that traffic comes in both directions
 	flag.Parse()
 	fmt.Printf("Forwarding TCP connections from: %s, to: %s\n", *src, *dst)
-	// TODO: listen until sigint is called, think about graceful shutdown
-	p := Proxy{
-		src: *src,
-		dst: *dst,
-		log: *log.Default(),
-	}
-	p.ServeTCP()
-}
-
-// todo add shutdown mechanism
-// serve listens for inbound TCP connections, creating a goroutine for each to forward connections
-func (p *Proxy) ServeTCP() error {
-	listener, err := net.Listen("tcp", p.src)
+	listener, err := net.Listen("tcp", *src)
 	if err != nil {
-		return fmt.Errorf("failed to listen %w", err)
+		log.Fatalf("failed to listen %s", err)
 	}
 	for {
 		// received new inbound connection
-		srcConn, err := listener.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
-			p.log.Printf("error accepting connection %s\n", err)
+			log.Printf("error accepting connection %s\n", err)
 			continue
 		}
 
 		go func() {
-			defer srcConn.Close()
-			dstConn, err := net.Dial("tcp", p.dst)
-			if err != nil {
-				p.log.Printf("failed to connect to dst %s\n", err)
-				return
-			}
-			defer dstConn.Close()
-			wg := sync.WaitGroup{}
-			// if either errors close the proxe
-			wg.Add(1)
-			copyWatcher(&wg, srcConn, dstConn)
-			copyWatcher(&wg, dstConn, srcConn)
-			wg.Wait()
+			p := NewProxy(*dst, *debug, log.Default())
+			p.ServeTCP(conn)
 		}()
 	}
 }
 
-// copyWatcher performs io.Copy() and decrements wg when it completes or returns an error
-// todo: logging should be optional
-// todo: each proxy connection should have a unique identifier
-func copyWatcher(wg *sync.WaitGroup, src io.Reader, dst io.Writer) {
-	io.Copy(os.Stdout, io.TeeReader(src, dst))
-	wg.Done()
+func NewProxy(dst string, debug bool, log *log.Logger) Proxy {
+	done := make(chan bool, 2)
+	return Proxy{
+		dst:   dst,
+		debug: debug,
+		done:  done,
+		log:   log,
+	}
+}
+
+// ServceTCP proxies a single connection copying data in bi-directionally
+func (p *Proxy) ServeTCP(srcConn net.Conn) {
+	p.srcConn = srcConn
+	defer p.srcConn.Close()
+
+	dstConn, err := net.Dial("tcp", *dst)
+	if err != nil {
+		log.Printf("failed to connect to dst %s\n", err)
+		return
+	}
+	p.dstConn = dstConn
+	defer dstConn.Close()
+
+	go p.pipe(p.srcConn, p.dstConn)
+	go p.pipe(p.dstConn, p.srcConn)
+	<-p.done
+}
+
+// pipe copies from src to dst, decrements Proxies wg when an error
+// is received to indicate connection has been closed. If debug is true
+// pipe will log all data copied
+func (p *Proxy) pipe(src net.Conn, dst net.Conn) {
+	logger := io.Discard
+	if p.debug {
+		prefix := "---->"
+		if src.LocalAddr() == p.dstConn.LocalAddr() {
+			prefix = "<----"
+		}
+		logger = connLogger{
+			prefix: prefix,
+			logger: p.log,
+		}
+	}
+	io.Copy(logger, io.TeeReader(src, dst))
+	p.done <- true
+}
+
+type connLogger struct {
+	prefix string
+	logger *log.Logger
+}
+
+func (cl connLogger) Write(p []byte) (n int, err error) {
+	cl.logger.Printf("%s %s", cl.prefix, string(p))
+	return len(p), nil
 }
